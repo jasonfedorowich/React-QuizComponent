0997b21331969c2d72a52add0d5e8a1a64ab0fc7
jasonfedorowich <jasonfedorowich@gmail.com>
2018-10-29 23:12:31 -0400

    Completed the module

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component{
+  constructor(props){
+    super(props)
+    this.state = { incorrectAnswer : false}
+  }
   handleClick(){
     this.props.clickHandler(this.props.button_text)
   }
